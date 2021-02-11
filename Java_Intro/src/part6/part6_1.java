7ec06bbe7f301365c815f8e3f3cbf969ba558ee8
IshiwatariRyo <arashi.takenoko86@gmail.com>
2021-02-11 16:25:42 +0900

    part6の追加
    part6が終了したため、コミットを行う

---
+++ Java_Intro/src/part6/part6_1.java
@@ -0,0 +1,26 @@
+package part6;
+
+public class part6_1 {
+public static void main(String[] args) throws Exception{
+	Zenhan.doWarusa();
+	Zenhan.doTogame();
+	Kouhan.callDeae();
+	Kouhan.showMondokoro();
+}
+/*
+public static void doWarusa() {
+	System.out.println("きなこでござる。食えませんがの");
+}
+public static void doTogame() {
+	System.out.println("この老いぼれの目はごまかせませんぞ");
+}
+public static void callDeae() {
+	System.out.println("ええい、であえい！！");
+}
+public static void showMondokoro() throws Exception{
+	System.out.println("飛車さん、角さん、もういいでしょう");
+	System.out.print("この紋所が目に入らぬか");
+	doTogame();
+}
+*/
+}
